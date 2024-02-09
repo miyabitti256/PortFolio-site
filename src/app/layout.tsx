6750c65e1b1c9a423c84973f772b5d0bc2d7b659
miyabitti <gilgameshmsayr1@gmail.com>
2024-02-09 10:29:07 +0900

    completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PortFolio",
-  description: "ポートフォリオサイト",
+  title: "PortFolio-miyabitti",
+  description: "miyabittiのポートフォリオサイト。 プログラミング初心者がNext.js、microCMSを使って作成しました。",
 };
 
 export default function RootLayout({
